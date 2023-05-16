ec77f2ed1a19b932999021bd5e234df25c6b0ab0
rjonczy <robert.jonczy@gmail.com>
2023-05-16 13:45:35 +0200

    updated gitsigns

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -7,9 +7,9 @@ gitsigns.setup {
   signs = {
     add          = { text = '+' },
     change       = { text = '~' },
-    delete       = { text = '-' },
-    topdelete    = { text = '-' },
-    changedelete = { text = '-' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
     untracked    = { text = '┆' },
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
