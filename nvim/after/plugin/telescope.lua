fa5fb3a8fe202087dc950c537e0ea97c0be35e04
rjonczy <robert.jonczy@gmail.com>
2023-07-21 13:25:10 +0200

    telescope live_grep in hidden files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,7 +2,12 @@ require('telescope').setup{
   pickers = {
     find_files = {
       hidden = true
-    }
+    },
+    live_grep = {
+      additional_args = function(opts)
+          return {"--hidden"}
+      end
+    },
   },
 }
 
